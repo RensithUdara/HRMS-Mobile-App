b511bb7519eb06e98d13c02540bb93524b05ad30
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-08-05 04:11:43 +0530

    Add MainScreen widget with bottom navigation and child widget support

---
+++ lib/views/main/main_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../controllers/auth_bloc.dart';
+import '../../config/app_theme.dart';
+
+class MainScreen extends StatefulWidget {
+  final Widget child;
+
+  const MainScreen({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.child,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          
+          // Navigate based on selected index
+          switch (index) {
+            case 0:
+              // Dashboard
+              break;
+            case 1:
+              // Attendance
+              break;
+            case 2:
+              // Leave
+              break;
+            case 3:
+              // Profile/More
+              break;
+          }
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppTheme.primaryColor,
+        unselectedItemColor: AppTheme.textSecondary,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: 'Attendance',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event_note),
+            label: 'Leave',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
